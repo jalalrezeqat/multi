6ae062df1d175d93e112fa3be1560d26031e3323
Jalal Rezeqat <47989412+jalalrezeqat@users.noreply.github.com>
2022-03-01 08:14:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/project/vendor/autoload.php';
+require __DIR__.'/app/project/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
