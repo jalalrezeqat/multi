d7d6015044c0518f0d426723cef833b3d9c28534
Jalal Rezeqat <47989412+jalalrezeqat@users.noreply.github.com>
2022-03-01 08:28:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ require __DIR__.'/app/project/vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/app/project/vendor/autoload.php';
+$app = require_once __DIR__.'/app/app/project/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
